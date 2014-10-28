1dd039c3b8b799438998deaba719df033dd48218
Vladimir Starkov <matmuchrapna@gmail.com>
2014-10-29 03:44:31 +0500

    feat(BEM): add some BEM to the project

---
+++ blocks/page/page.deps.js
@@ -0,0 +1,18 @@
+module.exports.require = [
+    { block: 'normalize' },
+    { block: 'print' },
+    { block: 'clearfix' },
+    { block: 'icheck' },
+    { block: 'nouislider' },
+];
+
+module.exports.expect = [
+    { block: 'header' },
+    { block: 'layout' },
+    { block: 'logo' },
+    { block: 'profile-form' },
+    { block: 'input-text' },
+    { block: 'input-radio' },
+    { block: 'profile-note' },
+    { block: 'input-check' },
+];
